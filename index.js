1033201af6b7389493ebd865fdbc179ef0b28850
Noah <noa316@naver.com>
2022-02-05 23:47:22 +0900

    ⚰[DEL] 사용하지 않을 라이브러리 제거

--- index.js
+++ index.js
@@ -8,11 +8,9 @@
 ================================================================================ */
 
 const { Client } = require("@notionhq/client")
-const dotenv = require("dotenv")
 const { Octokit } = require("octokit")
 const _ = require("lodash")
 
-dotenv.config()
 const octokit = new Octokit({ auth: process.env.GITHUB_KEY })
 const notion = new Client({ auth: process.env.NOTION_KEY })
 
